9b21b45f2c58ac63010680ebcb270d43d5865449
davidhennig <davidjhennig@gmail.com>
2020-04-22 15:20:42 -0400

    tuesday

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,7 +1,7 @@
+from dll_stack import Stack
+from dll_queue import Queue
 import sys
 sys.path.append('../queue_and_stack')
-from dll_queue import Queue
-from dll_stack import Stack
 
 
 class BinarySearchTree:
@@ -11,8 +11,25 @@ class BinarySearchTree:
         self.right = None
 
     # Insert the given value into the tree
+    # def insert(self, value):
+    #     if self is None:
+    #         self = BinarySearchTreeNode(value)
+    #     else:
+    #         if value < self.value:
+    #             self.left.insert(value)
+    #         else:
+    #             self.right.insert(value)
     def insert(self, value):
-        pass
+        if value < self.value:
+            if self.left:
+                self.left.insert(value)
+            else:
+                self.left = BinarySearchTree(value)
+        else:
+            if self.right:
+                self.right.insert(value)
+            else:
+                self.right = BinarySearchTree(value)
 
     # Return True if the tree contains the value
     # False if it does not
